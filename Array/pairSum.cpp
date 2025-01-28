4a112d80daeb23fd542115119752ca9cf89ad357
Rafikul Islam Hriday <hriday1833@gmail.com>
2025-01-28 19:15:53 +0600

    optimize pairSum

--- Array/pairSum.cpp
+++ Array/pairSum.cpp
@@ -2,26 +2,52 @@
 #include <vector>
 using namespace std;
 
-vector<int> pairSum(vector<int> nums, int target){
-    vector<int> ans; 
-    int n = nums.size();   
+vector<int> pairSum(vector<int> nums, int target)
+{
+    vector<int> ans;
+    int n = nums.size();
 
-    for(int i = 0; i<n; i++){
-        for(int j= i+1; j<n; j++){
-            if(nums[i]+nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
+    // approach 01
+    // for(int i = 0; i<n; i++){
+    //     for(int j= i+1; j<n; j++){
+    //         if(nums[i]+nums[j] == target){
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+
+    // approach 02 better one
+
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        int pairSum = nums[i] + nums[j];
+        if (pairSum < target)
+        {
+            i++;
+        }
+        else if (pairSum > target)
+        {
+            j--;
+        }
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
         }
     }
+
     return ans;
 }
 
-int main(){
-    vector<int> nums = {2,11,7,15};
-    int target = 9;
-    vector<int> ans = pairSum(nums,target);
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 26;
+    vector<int> ans = pairSum(nums, target);
 
     cout << ans[0] << " " << ans[1] << endl;
     return 0;
