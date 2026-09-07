262613f3fd46ae453d528a4595a586abdbd3a146
Rafikul Islam Hriday <hriday1833@gmail.com>
2026-09-07 18:01:00 +0600

    feat: add implementation for bubble sort algorithm

---
+++ Practice_Problem/Array/print_subarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] ={1,2,3,4,5};
+    int n = sizeof(arr) / sizeof(int);
+   
+     
+    for(int i=0; i<n; i++){ //
+        for(int j = i  ;  j <n ; j++){
+            for(int z = i ; z<= j ; z++){
+            cout << arr[z] << ",";
+            }
+            cout << "  " ;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
